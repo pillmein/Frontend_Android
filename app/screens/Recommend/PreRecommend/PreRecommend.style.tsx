2b6eee4ae3601b96a69a2cb96bdeec5ea28a3105
Ayun Jeong <ayun01j07@gmail.com>
2025-02-15 01:23:00 +0900

    feat #5: 추천 시작 뷰 구현

---
+++ app/screens/Recommend/PreRecommend/PreRecommend.style.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: #fff;
+`;
+
+export const Description = styled.Text`
+  text-align: right;
+  font-size: 16px;
+  color: gray;
+  margin-top: 20px;
+  margin-right: 15px;
+  margin-bottom: 35px;
+`;
+
+export const Checklist = styled.Text`
+  font-size: 16px;
+  margin-left: 10px;
+  margin-bottom: 5px;
+`;
+export const ButtonContainer = styled.View`
+  margin-top: 40px;
+`;
+export const Button = styled.TouchableOpacity`
+  background-color: #f0f0f0;
+  padding: 15px;
+  border-radius: 10px;
+  margin-vertical: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  text-align: center;
+  font-size: 16px;
+`;
