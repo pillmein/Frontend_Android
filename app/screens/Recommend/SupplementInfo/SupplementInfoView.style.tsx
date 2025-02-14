a1a4404a530dbcd1a59e9e0049bf8eabd0ce9ebe
Ayun Jeong <ayun01j07@gmail.com>
2025-02-15 01:25:20 +0900

    feat #5: 영양제 상세 정보 뷰 구현

---
+++ app/screens/Recommend/SupplementInfo/SupplementInfoView.style.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 16px;
+  background-color: #fff;
+`;
+
+export const InfoHeader = styled.View`
+  margin-top: 10px;
+  margin-bottom: 20px;
+  flex-direction: row;
+  position: relative;
+`;
+export const SupplementName = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  position: absolute;
+  left: 50%;
+  margin-left: -50px;
+`;
+export const InfoSection = styled.View`
+  margin-top: 20px;
+  margin-bottom: 40px;
+  padding: 8px;
+  background-color: #eff5ec;
+  border-radius: 10px;
+`;
+export const InfoLabel = styled.Text`
+  text-align: left;
+  padding: 8px;
+  background-color: #6a986c;
+  color: white;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+export const InfoList = styled.Text`
+  font-size: 16px;
+  padding: 5px;
+`;
