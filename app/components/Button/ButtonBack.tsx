cd10a10eeaefe96867d6e7a975a6b0c1e2994a0a
Ayun Jeong <ayun01j07@gmail.com>
2025-03-03 17:00:39 +0900

    fix: 뒤로가기 버튼 터치 가능하도록 수정

--- app/components/Button/ButtonBack.tsx
+++ app/components/Button/ButtonBack.tsx
@@ -1,22 +1,25 @@
-import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const ButtonBack = () => {
   const navigation = useNavigation();
-
   return (
-    <BackButton onPress={() => navigation.goBack()}>
-      <Ionicons name="chevron-back" size={25} />
+    <BackButton>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Ionicons name="chevron-back" size={25} />
+      </TouchableOpacity>
     </BackButton>
   );
 };
 
-const BackButton = styled.TouchableOpacity`
+export default ButtonBack;
+
+const BackButton = styled.View`
   width: 25px;
   height: 25px;
   justify-content: center;
   align-items: center;
+  z-index: 1;
 `;
-
-export default ButtonBack;
