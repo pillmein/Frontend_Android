b22637a9039a9c8514e20d583f9f49afdd834547
AyunJeong <ayun01j07@gmail.com>
2025-03-16 18:22:21 +0900

    feat: AI 분석 결과 view 추가

--- app/navigation/RecommendStack.tsx
+++ app/navigation/RecommendStack.tsx
@@ -1,11 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PreRecommendView from "../screens/Recommend/PreRecommend/PreRecommendView";
+import AnalysisResultView from "../screens/Recommend/AnalysisResult/AnalysisResultView";
 import RecommendView from "../screens/Recommend/RecommendView";
 import SupplementInfoView from "../screens/Recommend/SupplementInfo/SupplementInfoView";
 
 const Stack = createNativeStackNavigator<RecommendStackParamList>();
 type RecommendStackParamList = {
   PreRecommendView: undefined;
+  AnalysisResultView: undefined;
   RecommendView: undefined;
   SupplementInfoView: undefined;
 };
@@ -13,6 +15,7 @@ function RecommendStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="PreRecommendView" component={PreRecommendView} />
+      <Stack.Screen name="AnalysisResultView" component={AnalysisResultView} />
       <Stack.Screen name="RecommendView" component={RecommendView} />
       <Stack.Screen name="SupplementInfoView" component={SupplementInfoView} />
     </Stack.Navigator>
