abd018a13893c5cb21290bdb56be1c72a95795ad
AyunJeong <ayun01j07@gmail.com>
2025-04-02 21:29:13 +0900

    feat: axios 공통 인스턴스 구현, http 통신 허용 설정

---
+++ app/api/apiSO.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_BASE_URL_SO, ACCESS_TOKEN } from "@env";
+
+const apiSO = axios.create({
+  baseURL: API_BASE_URL_SO,
+  headers: {
+    'Authorization': `Bearer ${ACCESS_TOKEN}`,
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiSO;
--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,7 @@
 declare module "@env" {
   export const GOOGLE_WEB_CLIENT_ID: string;
   export const GOOGLE_IOS_CLIENT_ID: string;
+  export const API_BASE_URL_SO: string;
+  export const API_BASE_URL_SR: string;
+  export const ACCESS_TOKEN: string;
 }
