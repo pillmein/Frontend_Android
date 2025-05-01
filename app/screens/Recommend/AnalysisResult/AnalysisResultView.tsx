811aab518ac767ecbae88ddef3587f5c517509f6
AyunJeong <ayun01j07@gmail.com>
2025-05-01 22:18:34 +0900

    refactor +  mock-up data 주석 처리

--- app/screens/Recommend/AnalysisResult/AnalysisResultView.tsx
+++ app/screens/Recommend/AnalysisResult/AnalysisResultView.tsx
@@ -8,6 +8,18 @@ const AnalysisResultView = ({ navigation }: any) => {
   const [analysisSummary, setAnalysisSummary] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
 
+  // mock-up data
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setAnalysisSummary(
+  //       "1순위: 사용자님의 소화 문제와 집중력 문제가 가장 우려됩니다. 이러한 증상들은 각각 소화 기능 저하와 인지 기능 저하의 징후일 수 있습니다. 따라서 식사 시 프로바이오틱스와 비타민 B군을 함께 섭취하시는 것이 좋습니다. \n\n2순위: 두번째로, 눈의 피로와 시력 저하 문제가 중요합니다. 이러한 문제를 완화하기 위해서는 비타민 A와 루테인이 필요합니다. 이 두 영양소를 저녁 식사와 함께 복용하시는 것이 좋습니다.\n\n3순위: 마지막으로, 일상에서 느끼는 신체적 피로감이 있는데, 이는 철분과 비타민 B군, 그리고 마그네슘 섭취로 완화시킬 수 있습니다. 아침 식사와 함께 복용해보시기 바랍니다."
+  //     );
+  //     setLoading(false);
+  //   }, 2000);
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
   useEffect(() => {
     const fetchAnalysis = async () => {
       try {
